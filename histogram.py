48b29a46e3e76838181b00c4c61093301b207121
mchardin <mchardin@student.42.fr>
2022-09-14 17:52:03 +0200

    histo ok

---
+++ histogram.py
@@ -0,0 +1,21 @@
+from FileLoader import FileLoader
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+try:
+    assert len(sys.argv) > 1, "Input Error: missing argument"
+    fl = FileLoader()
+    df = fl.load(sys.argv[1])
+    courses = df.iloc[:, 6:].columns
+    print(courses)
+    fig, ax = plt.subplots(3, 5)
+    for i in range(0, len(courses)):
+        print(i//5,i%5)
+        sns.histplot(data=df, x=courses[i], ax=ax[i//5][i%5], hue="Hogwarts House", palette="rocket", element="step")
+    plt.show()
+except Exception as e:
+    print(e)
